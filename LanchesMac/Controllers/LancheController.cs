5d9a43eddd222a63188f55125c895dc3d306fb1b
Fernando Soares Neto <fernandonetosoares@hotmail.com>
2021-09-18 19:37:29 -0300

    Alter Images

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,33 @@
+﻿using LanchesMac.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, ICategoriaRepository categoriaRepository)
+        {
+            _lancheRepository = lancheRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+        public IActionResult List()
+        {
+            ViewBag.Lanche = "Lanches";
+            ViewData["Categoria"] = "Categoria";
+
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+            var lancheslistViewModel = new ViewModels.LancheListViewModel();
+            lancheslistViewModel.Lanches = _lancheRepository.Lanches;
+            lancheslistViewModel.CategoriaAtual = "Categoria Atual";
+            return View(lancheslistViewModel);
+        }
+    }
+}
